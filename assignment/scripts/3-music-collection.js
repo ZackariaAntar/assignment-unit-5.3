123c19bf870aef04798bca677602ad3ec620f591
Zackaria Antar <antarzack@gmail.com>
2023-04-24 16:58:42 -0500

    Capitalized my property names.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,9 +12,9 @@ let collection = []
 
 function addToCollection(title, artist, yearPublished){
      album ={
-          title: title,               
-          artist: artist,
-          ear: yearPublished
+          Title: title,               
+          Artist: artist,
+          Year: yearPublished
      };
           collection.push(album);
           return album;
@@ -24,20 +24,26 @@ function addToCollection(title, artist, yearPublished){
 //   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 //   - Console.log each album as added using the returned value.
 //   - After all are added, console.log the `collection` array.
+
 addToCollection("Babylon by Bus", "Bob Marley and the Wailers", "1978")
 console.log(album);
-// addToCollection("99.9%", "Kaytranada", "2016")
-// console.log(album);
-// addToCollection("Little Dragon", "Little Dragon", "2007")
-// console.log(album);
-// addToCollection("Cross", "Justice", "2007")
-// console.log(album);
-// addToCollection("BUBBA", "Kaytranada", "2019")
-// console.log(album);
-// addToCollection("Malibu", "Anderson .Paak", "2016")
-// console.log(album);
 
-console.log(collection);
+addToCollection("99.9%", "Kaytranada", "2016")
+console.log(album);
+
+addToCollection("Little Dragon", "Little Dragon", "2007")
+console.log(album);
+
+addToCollection("Cross", "Justice", "2007")
+console.log(album);
+
+addToCollection("BUBBA", "Kaytranada", "2019")
+console.log(album);
+
+addToCollection("Malibu", "Anderson .Paak", "2016")
+console.log(album);
+
+//console.log(collection);
 
 // - Add a function named `showCollection`. This function should:
 //   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
